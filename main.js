cf09c1dc75be95b48eaca36c760a8de67393f4f2
leo-dev22 <lndenko18@gmail.com>
2022-07-11 12:36:36 +0400

    files Updated

---
+++ main.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector(".nav");
+const mobileToggle = document.querySelector(".mobile-nav-toggle");
+
+console.log(nav, mobileToggle);
+mobileToggle.addEventListener("click", () => {
+  const visibility = nav.getAttribute("data-visible");
+  console.log(visibility);
+  if (visibility === "false") {
+    nav.setAttribute("data-visible", true);
+    mobileToggle.setAttribute("aria-expanded", true);
+  } else if (visibility === "true") {
+    nav.setAttribute("data-visible", false);
+    mobileToggle.setAttribute("aria-expanded", false);
+  }
+});
