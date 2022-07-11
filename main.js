6690c1d39f46d61613f078c225da01712495e40a
leopauld Ndenko <97967420+leo-dev22@users.noreply.github.com>
2022-07-11 13:15:27 +0400

    Update main .js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const nav = document.querySelector(".nav");
 const mobileToggle = document.querySelector(".mobile-nav-toggle");
 
-console.log(nav, mobileToggle);
+// console.log(nav, mobileToggle);
 mobileToggle.addEventListener("click", () => {
   const visibility = nav.getAttribute("data-visible");
-  console.log(visibility);
+//   console.log(visibility);
   if (visibility === "false") {
     nav.setAttribute("data-visible", true);
     mobileToggle.setAttribute("aria-expanded", true);
