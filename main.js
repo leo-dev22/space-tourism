1b61c0961898e3d7cb71e42f73f373c1f2b81299
leo-dev22 <lndenko18@gmail.com>
2022-07-20 21:33:32 +0400

    files updated

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const navigation = document.querySelector(".navigation-primary");
 const mobileToggle = document.querySelector(".mobile-nav-toggle");
 
-console.log(navigation, mobileToggle);
+// console.log(navigation, mobileToggle);
 mobileToggle.addEventListener("click", () => {
   const visibility = navigation.getAttribute("data-visible");
-  console.log(visibility);
+  // console.log(visibility);
   if (visibility === "false") {
     navigation.setAttribute("data-visible", true);
     mobileToggle.setAttribute("aria-expanded", true);
@@ -13,3 +13,73 @@ mobileToggle.addEventListener("click", () => {
     mobileToggle.setAttribute("aria-expanded", false);
   }
 });
+
+// const tabList = document.querySelector('[role="tablist"]');
+// const tabs = tabList.querySelectorAll('[role="tab"]');
+
+// tabList.addEventListener('keydown', changeTabFocus);
+
+// tabs.forEach((tab) => {
+//     tab.addEventListener('click', changeTabPanel);
+// });
+
+
+// let tabFocus = 0;
+// function changeTabFocus(e) {
+//     const keydownLeft = 37;
+//     const keydownRight = 39;
+    
+//     if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+//         tabs[tabFocus].setAttribute("tabindex", -1);
+//     }
+    
+//     if (e.keyCode === keydownRight) {
+//         tabFocus++;
+//         if (tabFocus >= tabs.length) {
+//             tabFocus = 0;
+//         }
+//     }
+    
+//     if (e.keyCode === keydownLeft) {
+//         tabFocus--;
+//         if (tabFocus < 0) {
+//             tabFocus = tabs.length - 1;
+//         }
+//     }
+    
+//     tabs[tabFocus].setAttribute("tabindex", 0);
+//     tabs[tabFocus].focus();
+// }
+
+
+// function changeTabPanel(e) {
+//     const targetTab = e.target;
+//     const targetPanel = targetTab.getAttribute("aria-controls");
+//     const targetImage = targetTab.getAttribute("data-image");
+    
+//     const tabContainer = targetTab.parentNode;
+//     const mainContainer = tabContainer.parentNode;
+    
+//     tabContainer
+//         .querySelector('[aria-selected="true"]')
+//         .setAttribute("aria-selected", false);
+        
+//     targetTab.setAttribute("aria-selected", true);
+    
+//     hideContent(mainContainer, '[role="tabpanel"]');
+//     showContent(mainContainer, [`#${targetPanel}`]);
+    
+//     hideContent(mainContainer, 'picture');
+//     showContent(mainContainer, [`#${targetImage}`]);
+// }
+
+// function hideContent(parent, content) {
+//     parent
+//         .querySelectorAll(content)
+//         .forEach((item) => item.setAttribute("hidden", true));
+// }
+
+// function showContent(parent, content) {
+//      parent.querySelector(content).removeAttribute('hidden');
+// }
+
